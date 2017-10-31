d5c921fb336d213dcc57173c2e99c588a3f9cba9
David Holovii <mirage.present@gmail.com>
2017-10-31 11:25:52 +0200

    Updated ide-helper file

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.19 on 2017-10-30.
+ * Generated for Laravel 5.5.19 on 2017-10-31.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
@@ -14195,6 +14195,12 @@ namespace  {
 
     class User extends \App\Models\User {}
 
+    class Account extends \App\Models\Account {}
+
+    class Pocket extends \App\Models\Pocket {}
+
+    class Transaction extends \App\Models\Transaction {}
+ 
 }
 
 
